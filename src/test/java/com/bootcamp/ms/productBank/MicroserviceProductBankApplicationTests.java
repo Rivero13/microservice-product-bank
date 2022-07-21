bffb2b6952dff3bdba3c463150294f23e146c9c3
Rivero13 <rivero_jair@hotmail.com>
2022-07-21 12:51:36 -0500

    pruebas junit

--- src/test/java/com/bootcamp/ms/productBank/MicroserviceProductBankApplicationTests.java
+++ src/test/java/com/bootcamp/ms/productBank/MicroserviceProductBankApplicationTests.java
@@ -1,13 +1,40 @@
 package com.bootcamp.ms.productBank;
 
+import com.bootcamp.ms.commons.entity.ProductBank;
+import com.bootcamp.ms.productBank.service.ProductBankService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+//@SpringBootTest
 class MicroserviceProductBankApplicationTests {
 
+	private final ProductBank productBank = ProductBank.builder()
+			.id("12345")
+			.description("cuentas corrientes")
+			.build();
+
 	@Test
-	void contextLoads() {
+	void create() {
+		assertNotNull(productBank);
+	}
+
+	@Test
+	void findById() {
+		assertAll(
+				() -> assertNotNull(productBank.getId()),
+				() -> assertEquals("12345", productBank.getId())
+		);
+	}
+
+	@Test
+	void findByName() {
+		assertAll(
+				() -> assertNotNull(productBank.getDescription()),
+				() -> assertEquals("cuentas corrientes", productBank.getDescription())
+		);
 	}
 
 }
